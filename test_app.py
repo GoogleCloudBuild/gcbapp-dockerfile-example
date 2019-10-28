a9e53ca3e3d2236b8b482941e93bb7addf1a997e
Svetlana-Panamarenka <svetlana_panamarenka@epam.com>
2019-10-28 20:05:01 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Hello World!\n")
+    self.assertEqual(hello(), "Hello World!!\n")
 
 if __name__ == '__main__':
   unittest.main()
